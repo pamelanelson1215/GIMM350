a6caa687da3aa8b14ae8b4332738b9fc5aa0d9cf
pamelanelson1215 <54724859+pamelanelson1215@users.noreply.github.com>
2019-09-05 11:32:46 -0600

    Create Utilities.cs
    code from challenge 1

---
+++ Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities
+{
+    //static class that won't be in the actual scene
+
+    public static string ProcessText(string input)
+    {
+        //my code goes here
+        //determine whether user has input a word or a number
+        //return the string "word" if user enters a word (includes 'ab.23cd')
+        //return the string "number" if user enters a number (inludes 1.3)
+
+        //try catch method to try for numerical value
+        try
+        {
+            double number = double.Parse(input);
+            return "number";
+        }
+
+        catch
+        {
+            return "word";
+        }
+    }
+}
